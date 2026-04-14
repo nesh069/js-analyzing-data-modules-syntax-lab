d339c3ab6735a15948c4bd7d38031cd61ef795dd
Emmanuel Munene <emmanuel.munene@student.moringaschool.com>
2026-04-14 21:22:19 +0300

    Lab assesment

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+const dateJS = require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
 
+  for (const arr of args) {
+    combinedObject.users = [...combinedObject.users, ...arr];
+  }
+
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+  return combinedObject;
+}
+
+module.exports = combineUsers;
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
